138f18e8d820f5d02757935f6b3358fadca08324
devMatheusR <devmatheusr@gmail.com>
2023-07-05 17:15:40 -0300

    initial

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+};
